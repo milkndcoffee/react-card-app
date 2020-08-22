c310873c7e4f1e5967040fb65ce2ec478ea408a7
milkndcoffee <milkndcoffee@gmail.com>
2020-08-21 19:15:28 -0500

    finished applying Card and UserComment component onto USerCommentGrid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import UserComment from './components/UserComment';
+import UserCommentGrid from './components/UserCommentGrid';
+import Header from './components/ui/Header';
 
 import './App.css';
 
@@ -9,9 +10,21 @@ function App() {
     cardData: [
       {
         id: 0,
-        img: 'pictures/flowers-5479950_1920.jpg',
-        title: 'Flowers!',
-        desc: 'A beautiful picture containing flowers flowering in the foreground relished by grasses.'
+        img: 'https://images.unsplash.com/photo-1455659817273-f96807779a8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        title: 'Sunflowers!',
+        desc: 'A beautiful picture containing a bright surplus of sunflowers'
+      },
+      {
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1546201483-a0fa9e5ed258?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        title: 'Bulldawg',
+        desc: 'Look at this photogenic bulldog! One of the cutest in the world'
+      },
+      {
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1519077204685-ed90d0cc05b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80',
+        title: 'Ramen',
+        desc: 'What makes ramen so unique from other foods? Click on the article and find out now!'
       }
     ],
     userData: [
@@ -21,14 +34,38 @@ function App() {
         username: 'fakeguy01',
         comment: 'woah, these flowers r cool',
         date: '08-15-2020'
+      },
+      {
+        id: 1,
+        cardId: 2,
+        username: 'ramenlover',
+        comment: 'ugh, this article is all wrong!! what a disgrace to ramen lovers all around the world!!!',
+        date: '08-15-2020'
+      },
+      {
+        id: 2,
+        cardId: 1,
+        username: 'sophine',
+        comment: 'i need me one of these',
+        date: '08-14-2020'
+      },
+      {
+        id: 3,
+        cardId: 0,
+        username: '5unfl0w3r_h4t3r',
+        comment: 'so ugly lmao wym @fakeguy01',
+        date: '08-21-2020'
       }
     ]
   }
 
+  //.<UserComment item={state.userData} />
+  
 
   return (
     <div className="container">
-      <UserComment item={state.userData} />
+      <Header />
+      <UserCommentGrid items={state} />
     </div>
   );
 }
--- src/components/UserComment.js
+++ src/components/UserComment.js
@@ -3,44 +3,55 @@ import React from 'react'
 //TODO: Have a grid system so that i 
 //will not utilize arrays here but on another component
 
-const UserComment = ({ item }) => {
-  return (
-    <div style={wholeCardContainer}>
-      <img alt='profile-pic' style={profilePicStyle}></img>
-      <div style={containStyle}>
+const UserComment = ({ item, cardData }) => {
 
-        <div>
-          <div style={topStyle}>
-            <p style={usernameStyle}>{item[0].username}</p>
-            <p>{item[0].date}</p>
-          </div>
-          <p>{item[0].comment}</p>
-          <div style={cardPlacementStyle}>
-            <img alt='card-img' src={item[0].img}></img>
-          </div>
+  return (
+    <div className='userComment'>
+
+      <div style={userContainerStyle}>
+        <img alt='profile-pic' style={profilePicStyle}></img>
+        <div style={containStyle}>
           <div>
-           <p>CARD INFO</p>
+            <div style={{ paddingLeft: '1rem' }}>
+              <div style={topStyle}>
+                <p style={usernameStyle}>{item.username}</p>
+                <p style={{ fontSize: '8pt', color: 'gray' }}>{item.date}</p>
+              </div>
+              <p style={{ marginTop: '-.6rem', fontFamily: 'calibri' }}>{item.comment}</p>
+            </div>
+
+            {cardData}
+
           </div>
         </div>
-      </div>
 
+      </div>
     </div>
+
   )
 }
 
-const wholeCardContainer = {
+//Stylings
+const userContainerStyle = {
   display: 'flex',
   justifyContent: 'start',
-  flexDirection: 'row'
+  flexDirection: 'row',
+  padding: '.2rem',
+  width: '532px',
+  margin: '1rem'
 }
+
 const profilePicStyle = {
-  margin: '1rem',
+  margin: '.2rem',
+  marginRight: '.3rem',
+  width: '50px',
+  height: '50px',
+  borderRadius: '30px',
+  background: 'gray'
 }
 const containStyle = {
-  background: '#f4f4f4'
 }
 const topStyle = {
-  background: 'gray',
   display: 'inline-flex',
   alignItems: 'center'
 }
@@ -49,11 +60,6 @@ const usernameStyle = {
   fontWeight: 'bold',
   paddingRight: '1rem'
 }
-const cardPlacementStyle={
-  background: 'black',
-  width: '440px',
-  height: '240px',
-  borderRadius: '6px'
-}
+
 
 export default UserComment
